04c77f5e1a483d5b8645998b45677101634e5304
Sarit Soponvattanasevi <kimmix05@gmail.com>
2018-11-11 19:50:30 +0700

    Error handling3

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const User = require("../models").User;
+const express = require('express');
+const User = require('../models').User;
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 //Login
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { email, password } = req.body;
   User.findOne({ where: { user_email: email } }).then(user => {
     if (user) {
       bcrypt.compare(password, user.user_password, (err, result) => {
         if (result) res.json({ user: user.toAuthJSON() });
-        else res.status(500).sent({ error: "Invalid password" });
+        else res.status(500).sent('Invalid password');
       });
-    } else res.status(500).sent({ error: "User not found" });
+    } else res.status(500).sent('User not found');
   });
 });
 
