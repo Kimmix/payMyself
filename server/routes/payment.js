3428e6d8c7a39a9e4ff0233435f7149a3babde04
Kimmix-W10 <kimmix05@gmail.com>
2018-12-16 17:10:16 +0700

    fix2

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   Payment.findAll({
     where: { user_fk: req.currentUser.user_id },
     limit: 10,
-    order: 'payment_id DESC'
+    order: ['payment_id DESC']
   })
     .then(payment => res.status(200).json(payment))
     .catch(error => res.status(500).send(error));
