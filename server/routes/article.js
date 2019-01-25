ea721eea819a1a49c763a839f21c5c185e0ccc70
Sarit Soponvattanasevi <kimmix05@gmail.com>
2019-01-25 10:31:15 +0700

    fix route url, add article route

--- server/routes/article.js
+++ server/routes/article.js
@@ -9,13 +9,40 @@ router.get('/', (req, res) => {
     .catch(error => res.status(500).send(error));
 });
 
-router.post('/product', auth, (req, res) => {
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Article.findById(id)
+    .then(article => {
+      if (!article) return res.status(500).send('Article Not Found');
+      else res.status(200).send(article);
+    })
+    .catch(error => res.status(500).send(error));
+});
+
+router.post('/', auth, (req, res) => {
   const { title, excerpt, content } = req.body;
   Article.create({
     user_fk: req.currentUser.user_id,
     title, excerpt, content
   })
     .then(article => res.status(201).json(article.title + ' created'))
+    .catch(error => res.status(500).send(error));
+});
+
+router.put('/:id', auth, (req, res) => {
+  const { id } = req.params;
+  const { title, excerpt, content } = req.body;
+  Article.findById(id)
+    .then(article => {
+      if (article.user_fk === req.currentUser.user_id) {
+        article
+          .update({ title, excerpt, content })
+          .then(article =>
+            res.status(203).json(article.title + ' updated')
+          );
+      }
+      else res.status(500).send('Article Not Found');
+    })
     .catch(error => res.status(500).send(error));
 });
 
