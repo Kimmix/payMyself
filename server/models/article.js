aca752d72af011cecd66587cd57f865a2982af6f
Sarit Soponvattanasevi <kimmix05@gmail.com>
2019-01-25 10:51:43 +0700

    hot fix

--- server/models/article.js
+++ server/models/article.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Payment = sequelize.define("Article", {
+  const Article = sequelize.define("Article", {
     article_id: {
       allowNull: false,
       primaryKey: true,
